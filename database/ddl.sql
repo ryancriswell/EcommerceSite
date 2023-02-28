301b9463301e98f2612ad6c9d97bbe0ae2de8778
Fuzzyzilla <fuzzyzilla01@gmail.com>
2023-02-28 12:41:47 -0800

    Include Logging and Session cookies, syntax check

--- database/ddl.sql
+++ database/ddl.sql
@@ -16,7 +16,7 @@ Create Table AdministratorPermission (
 );
 
 Create Table User (
-  user_id int primary key AUTO_INCREMENT; 
+  user_id int primary key AUTO_INCREMENT,
   username varchar(16) not null,
   register_date datetime default CURRENT_TIMESTAMP,
   salt int not null,
@@ -26,9 +26,9 @@ Create Table User (
 Create Table CustomerPayment (
   user_id int,
   card_number char(16) not null,
-  full_name varchar(64), --nullable
+  full_name varchar(64), -- nullable
   security_code char(3) not null,
-  primary key(user_id, card_number), --One card/many user and one user/many card relationships allowed
+  primary key(user_id, card_number), -- One card/many user and one user/many card relationships allowed
   foreign key (user_id) references User (user_id)
     on delete cascade
     on update cascade
@@ -37,7 +37,7 @@ Create Table CustomerShipping (
   user_id int,
   state char(2) not null,
   addr_line_1 varchar(64) not null,
-  addr_line_2 varchar(64), --nullable
+  addr_line_2 varchar(64), -- nullable
   zip char(5) not null,
   primary key (user_id, state, addr_line_1, addr_line_2, zip),
   foreign key (user_id) references User
@@ -46,17 +46,17 @@ Create Table CustomerShipping (
 );
 Create Table Item (
   item_id int primary key AUTO_INCREMENT,
-  item_name varchar,
+  item_name varchar(50),
   item_description text,
-  price decimal(6,2), --2 decimal points, 6 digits ($9999.99 coffee anyone?)
+  price decimal(6,2), -- 2 decimal points, 6 digits ($9999.99 coffee anyone?)
   in_stock int UNSIGNED,
   CHECK(price >= 0.0)
 );
-Create Table Order (
+Create Table UserOrder (
   order_id int primary key AUTO_INCREMENT,
   user_id int not null,
   foreign key (user_id) references User
-    on delete set null --We want to remember previous orders even if the user deletes their account.
+    on delete set null -- We want to remember previous orders even if the user deletes their account.
     on update cascade
 );
 Create Table OrderItem (
@@ -65,19 +65,46 @@ Create Table OrderItem (
   quantity int UNSIGNED not null,
   total_price decimal(7, 2) not null,
   primary key (order_id, item_id),
-  foreign key (order_id) references Order
+  foreign key (order_id) references UserOrder
     on delete cascade
     on update cascade,
   foreign key (item_id) references Item
-    on delete set null, --Remember deleted items on orders
+    on delete set null -- Remember deleted items on orders
     on update cascade
 );
 Create Table RecurringOrder (
-  order_id int not null
-  recur_period_days int not null
-  first_date date not null
-  final_date date --nullable - perpetual subscription
-  foreign key (order_id) references Order
-    on delete restrict --Orders should not be deleted while ongoing! Cancel subscription first.
+  order_id int not null,
+  recur_period_days int not null,
+  first_date date not null,
+  final_date date, -- nullable - perpetual subscription
+  foreign key (order_id) references UserOrder
+    on delete restrict -- Orders should not be deleted while ongoing! Cancel subscription first.
     on update cascade
 );
+
+Create Table LogEntry {
+  description text not null,
+  username varchar(16), -- relavent user, if any [ref: > User.username]
+  order_id int, -- relavent order, if any [ref: > Order.order_id]
+  item_id int, -- relavent item, if any [ref: > Item.item_id]
+  log_date datetime default CURRENT_TIMESTAMP
+  foreign key username references User (username)
+	on delete set null
+	on update cascade
+  foreign key order_id references UserOrder (order_id)
+	on delete set null
+	on update cascade
+  foreign key item_id references Item (item_id)
+	on delete set null
+	on update cascade
+}
+Create Table SessionCookie {
+  username varchar(16), [ref: > User.username]
+  cookie char(64),
+  expires datetime,
+
+  primary key (username, cookie),
+  foreign key username references User (username)
+	on delete cascade
+	on update cascade
+}
