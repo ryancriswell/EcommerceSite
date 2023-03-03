9f50edd56a31668421da4fa198ace4c00eca0d6c
fuzzyzilla <fuzzyzilla01@gmail.com>
2023-03-03 11:45:35 -0800

    Comment Shop.js

--- App/src/components/Shop.js
+++ App/src/components/Shop.js
@@ -24,41 +24,47 @@ export default class Shop extends React.Component {
     render() {
         const [itemList, setItemList] = useState(null);
 
+        // Request the list of items from the server
         useEffect(() => {
             fetch("/shopitems")
                 .then((list) => setItemList(JSON.parse(list)))
                 .catch(() => setItemList([]));
         }, []);
 
+        // Request not satisfied yet, wait...
         if (itemList === null) {
             return <p>Loading...</p>;
         }
 
+        // Make a list of rows
         var rows = Math.floor(itemList.length() / 3);
-
         var htmlRows = [];
 
+        // Generate Html...
         for(var i = 0; i < rows; ++i) {
             let items = [];
 
+            // Three items per row
             for(var j = i * 3; j < i * 3 + 3; ++j) {
+                //Instantiate an Item component
                 let thisItem = itemList[j];
                 items.push(
                     <Item key={j} title={thisItem.name} />
                 );
             }
 
+            //Push the whole row
             htmlRows.push(
                 <section key={i} className="showcase-container">
                 </section>
             );
         }
 
+
+        // Return built page
         return (
             <div>
-                <div>
                     {htmlRows}
-                </div>
             </div>
         );
     }
