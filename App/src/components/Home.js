303bac60eee700669db328f59d94babdc2d87c6b
Fuzzyzilla <fuzzyzilla01@gmail.com>
2023-03-02 18:49:05 -0800

    Make Homepage more modular with components

--- App/src/components/Home.js
+++ App/src/components/Home.js
@@ -1,47 +1,36 @@
-
+import React from "react";
 export default function Home(){
+
+    class ItemShowcase extends React.Component {
+        render() {
+            return <div className="item-showcase">
+                    <h1 className="section-header">{this.props["title"]}</h1>
+                    <img src={this.props["image-src"]} alt={this.props["alt"]} className="item-image"/>
+                    <p className="section-boldline">{this.props["boldline"]}</p>
+                    <p className="section-text">
+                    {this.props["content"]}
+                    </p>
+                </div>
+        }
+    }
+
+    var lipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore
+    magna aliqua. Lacus vestibulum sed arcu non
+    odio euismod. Nibh tortor id aliquet lectus
+    proin nibh nisl condimentum id.`;
+
     return(
         <div id="content">
             <section className="showcase-container" id="best-choices">
-                <div className="item-showcase">
-                    <h1 className="section-header">Top Review</h1>
-                    <img src="../images/Mocha Bag.png" alt="Mocha Bean coffee" className="item-image"/>
-                    <p className="section-boldline">Tod Smith</p>
-                    <p className="section-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Lacus vestibulum sed arcu non
-                        odio euismod. Nibh tortor id aliquet lectus
-                        proin nibh nisl condimentum id.
-                    </p>
-                </div>
-                <div id="item-showcase">
-                    <h1 className="section-header">Owner's Choice</h1>
-                    <img src="../images/Robusta-Bean.png" alt="Robusta Bean coffee" className="item-image"/>
-                    <p className="section-boldline">The Lads Favorite</p>
-                    <p className="section-text">
-                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Lacus vestibulum sed arcu non
-                        odio euismod. Nibh tortor id aliquet lectus
-                        proin nibh nisl condimentum id.
-                    </p>
-                </div>
+                <ItemShowcase title="Top Review" boldline="Tod Smith" image-src="../images/Mocha Bag.png" alt="Mocha Bean coffee" content={lipsum} />
+                <ItemShowcase title="Owner's Choice" boldline="The Lads Favorite" image-src="../images/Colombia-Bean.png" alt="Colombia Bean coffee" content={lipsum} />
             </section>
             <section className="showcase-container" id="top-sellers">
                 <h1 className="section-header">Top Sellers</h1>
-                <div className="item-showcase">
-                    <h1 className="section-header">1.</h1>
-                    <img src="../images/Aztec-Bean.png" alt="Aztec Bean coffee" className="item-image"/>
-                </div>
-                <div className="item-showcase">
-                    <h1 className="section-header">2.</h1>
-                    <img src="../images/Brazil-Beans.png" alt="Brazil Bean coffee" className="item-image"/>
-                </div>
-                <div className="item-showcase">
-                    <h1 className="section-header">3.</h1>
-                    <img src="../images/TheclassNameic-Bean.png" alt="The classNameic Bean coffee" className="item-image"/>
-                </div>
+                <ItemShowcase title="1." image-src="../images/Aztec-Bean.png" alt="Aztec Bean coffee" />
+                <ItemShowcase title="2." image-src="../images/Brazil-Beans.png" alt="Brazil Bean coffee" />
+                <ItemShowcase title="3." image-src="../images/Decaf.png" alt="Decaf coffee" />
             </section>
         </div>
     );
