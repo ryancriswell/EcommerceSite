bffef4157ce8e6618f8c0413acd542c6a3c30d2a
Fuzzyzilla <fuzzyzilla01@gmail.com>
2023-03-02 19:19:11 -0800

    Skeleton Footer

--- App/src/App.js
+++ App/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
 import Shop from "./components/Shop";
+import Footer from "./components/Footer";
 
 import {Routes, Route} from "react-router-dom";
 
@@ -16,6 +17,7 @@ return (
         <Route path="/shop" element={<Shop/>}/>
         {/* ADD MORE ROUTES HERE!! */}
       </Routes>
+      <Footer />
     </div>
   );
 }
