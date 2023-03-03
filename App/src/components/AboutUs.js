f0f998ed1b8f8f13312e0c484d60f72a1692c1cb
Fuzzyzilla <fuzzyzilla01@gmail.com>
2023-03-02 18:54:38 -0800

    More incorrect `class` syntax fixes

--- App/src/components/AboutUs.js
+++ App/src/components/AboutUs.js
@@ -2,17 +2,17 @@
 export default function AboutUs(){
     return(
         <div>
-            <section class="aboutus--row">
-                <div class="aboutus--column">
-                    <h1 class="aboutus--h1">Redefining what it means to brew great coffee.</h1>
+            <section className="aboutus--row">
+                <div className="aboutus--column">
+                    <h1 className="aboutus--h1">Redefining what it means to brew great coffee.</h1>
                 </div>
-                <div class="aboutus--column">
-                    <img src="../images/hotcupwithbeans.jpeg" alt="A cup of coffee" class="aboutus--img"/>
+                <div className="aboutus--column">
+                    <img src="../images/hotcupwithbeans.jpeg" alt="A cup of coffee" className="aboutus--img"/>
                 </div>
             </section>
 
-            <section class="aboutus--row">
-                <div class="aboutus--column" id="aboutus--subscription-wrapper">
+            <section className="aboutus--row">
+                <div className="aboutus--column" id="aboutus--subscription-wrapper">
                     <h1 id="aboutus--subscription-title">How does the subscription work?</h1>
                     <h4 id="aboutus--subscription-body">We know it can be overwhelming to choose your coffee before experiencing that fresh ground smell first.
                         Our subscription program allows you to try a different coffee every month, or if you are a conniseur
@@ -22,13 +22,13 @@ export default function AboutUs(){
                 </div> 
             </section>
 
-            <section class="aboutus--row">
-                <div class="aboutus--column">
-                    <img src="../images/coffeepeople.jpeg" alt="People drinking coffee and smiling" class="aboutus--img"/>
+            <section className="aboutus--row">
+                <div className="aboutus--column">
+                    <img src="../images/coffeepeople.jpeg" alt="People drinking coffee and smiling" className="aboutus--img"/>
                 </div>
-                <div class="aboutus--column">
-                    <h1 class="aboutus--h1">Our Mission</h1>
-                    <h3 class="aboutus--h3">We want to connect our coffee with as many hearts as possible. There are billions of people drinking
+                <div className="aboutus--column">
+                    <h1 className="aboutus--h1">Our Mission</h1>
+                    <h3 className="aboutus--h3">We want to connect our coffee with as many hearts as possible. There are billions of people drinking
                         filthy, nasty, rotten, bunk coffee beans from some of our competitiors because they're simply clueless. We are 
                         going to show them what it means to have a good cup of coffee even if it kills us.
                     </h3>
--- App/src/components/Shop.js
+++ App/src/components/Shop.js
@@ -3,61 +3,61 @@ export default function Shop(){
     return (
         <div>
             <div>
-                <section class="showcase-container" id="Azetec-Bean">
-                    <div class="item-showcase">
-                        <h1 class="section-header">Azetec Bean</h1>
-                        <img src="../images/Aztec-Bean.png" alt="Azetec Bean coffee" class="item-image" width="500"/>
-                        <p class="section-text"></p>
+                <section className="showcase-container" id="Azetec-Bean">
+                    <div className="item-showcase">
+                        <h1 className="section-header">Azetec Bean</h1>
+                        <img src="../images/Aztec-Bean.png" alt="Azetec Bean coffee" className="item-image" width="500"/>
+                        <p className="section-text"></p>
                         <div>
                             <button>
-                                <span class="add-to-cart">Add To cart</span>
+                                <span className="add-to-cart">Add To cart</span>
                             </button>
                         </div>
                     </div>
                     <div id="item-showcase">
-                        <h1 class="section-header">Brazil Bean</h1>
-                        <img src="../images/Brazil-Beans.png" alt="Brazil Bean coffee" class="item-image" width="500"/>
-                        <p class="section-text"></p>
+                        <h1 className="section-header">Brazil Bean</h1>
+                        <img src="../images/Brazil-Beans.png" alt="Brazil Bean coffee" className="item-image" width="500"/>
+                        <p className="section-text"></p>
                         <div>
                             <button>
-                                <span class="add-to-cart">Add To cart</span>
+                                <span className="add-to-cart">Add To cart</span>
                             </button>
                         </div>
                     </div>
                     <div id="item-showcase">
-                        <h1 class="section-header">Mocha Bag</h1>
-                        <img src="../images/Mocha Bag.png" alt="Mocha Bag" class="item-image" width="500"/>
-                        <p class="section-text"></p>
+                        <h1 className="section-header">Mocha Bag</h1>
+                        <img src="../images/Mocha Bag.png" alt="Mocha Bag" className="item-image" width="500"/>
+                        <p className="section-text"></p>
                         <div>
                             <button>
-                                <span class="add-to-cart">Add To cart</span>
+                                <span className="add-to-cart">Add To cart</span>
                             </button>
                         </div>
                     </div>
                 </section>
-                <section class="showcase-container" id="Robusta-Bean">
-                    <div class="item-showcase">
-                        <h1 class="section-header">Robusta Bean</h1>
-                        <img src="../images/Robusta-Bean.png" alt="Robusta Bean coffee" class="item-image" width="500"/>
-                        <p class="section-text"></p>
+                <section className="showcase-container" id="Robusta-Bean">
+                    <div className="item-showcase">
+                        <h1 className="section-header">Robusta Bean</h1>
+                        <img src="../images/Robusta-Bean.png" alt="Robusta Bean coffee" className="item-image" width="500"/>
+                        <p className="section-text"></p>
                         <div>
                             <button>
-                                <span class="add-to-cart">Add To cart</span>
+                                <span className="add-to-cart">Add To cart</span>
                             </button>
                         </div>
                     </div>
                     <div id="item-showcase">
-                        <h1 class="section-header">Owner's Choice</h1>
-                        <img src="../images/TheClassic-Bean.png" alt="The Classic Bean coffee" class="item-image" width="500"/>
-                        <p class="section-text"></p>
+                        <h1 className="section-header">Owner's Choice</h1>
+                        <img src="../images/TheclassNameic-Bean.png" alt="The classNameic Bean coffee" className="item-image" width="500"/>
+                        <p className="section-text"></p>
                         <div>
                             <button>
-                                <span class="add-to-cart">Add To cart</span>
+                                <span className="add-to-cart">Add To cart</span>
                             </button>
                         </div>
                     </div>
                 </section>
-                <section class="showcase-container" id="top-sellers">
+                <section className="showcase-container" id="top-sellers">
                 </section><script src="https://kit.fontawesome.com/ff85fda485.js" crossorigin="anonymous"></script>    
             </div>
         </div>
