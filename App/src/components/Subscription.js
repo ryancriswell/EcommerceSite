d5fd2eb175dec7da94ea8a8e6001fa05dce816b2
Logan Stoltz <lstoltz@ewu.edu>
2023-03-07 14:10:17 -0800

    Update to subscription page

--- App/src/components/Subscription.js
+++ App/src/components/Subscription.js
@@ -7,7 +7,7 @@ export default function subscription(){
                     <h1 id="subscription-title">Lads<br/>Coffee<br/>Subscription</h1>
                 </div>
                 <div className="subscription-column">
-                    <h2 id="subscription-body">
+                    <h2 id="subscription-body" font-size="3vw">
                         Can't get enough of our fabulous coffee? Never run out of our one of a kind beans ever again when you subscribe!
                         Start each morning without the stress of missing out on a fresh cup of coffee. Once you subscribe the Lads will handle it all.
                     </h2>
@@ -15,10 +15,10 @@ export default function subscription(){
             </section>
 
             <section id="subscription-wrapper">
-                <h3 id="subscription-second-title">How does it Work?</h3><hr/>
+                <h3 id="subscription-second-title" font-size="2vw">How does it Work?</h3><hr/>
                 <div className="subscription-row">
                     <div className="subscription-column">
-                        <img src="..\images\Coffee-bean-1a.png" className="subscription-icons"/> 
+                        <img src="..\images\Coffee-bean-1.png" className="subscription-icons"/> 
                         <div className="subscription-column">
                             <h4>select your favorite bags of coffee</h4>
                         </div>
@@ -38,12 +38,12 @@ export default function subscription(){
                 </div> 
             </section>
 
-            <section className="subscription-row" >
+            <section id="subscription-wrapper" className="subscription-row" >
                 <div className="subscription-column">
-                <img src="..\images\Student-Essentials.png"/> 
+                    <img src="..\images\Student-Essentials.png" className="subscription-image"/> 
                 </div>
-                <div className="subscription-column">
-
+                <div className="coffee-subscription-box">
+                    <h2 id="subscription-second-title">Lads Coffee Subscription</h2>
                 </div>
             </section>
         </div>
