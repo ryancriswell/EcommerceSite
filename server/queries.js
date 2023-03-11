b222eec5825d7c264be9d97d2b9c7786d2864aec
J0seph0 <jhoward26@ewu.edu>
2023-03-11 12:55:46 -0800

    Connected to node and sql!

--- server/queries.js
+++ server/queries.js
@@ -1,26 +1,16 @@
-const mysql = require("promise-mysql");
+const mysql = require('mysql');
 
-//Throw if connection failed, unwrap the connection if successful.
-/*
-var con = mysql.createConnection({
-    host: "mysql://localhost:3306",
-    user: "admin",
-    // No one will ever guess this password stored in a public GitHub!!
-    password: "ReallyCoolSecurePassword!123"
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'node',
+  password: 'ReallyCoolSecurePassword!123',
+  database: 'ecommerce_db'
 });
 
-function parseSqlJson(result) {
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
-}
-/*
-export default function getItems(con) {
-    con.query("SELECT * from Item", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });//end of con.query
-}
-async function executeGetJson(con, query) {
-    return con.query(query)
-        .then(parseSqlJson);
-}
-*/
+
