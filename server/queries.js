94ff9aa4b39fbbc1ac5b5a9f3f9a33dee729d121
J0seph0 <jhoward26@ewu.edu>
2023-03-11 13:10:58 -0800

    working query now connect to website

--- server/queries.js
+++ server/queries.js
@@ -7,10 +7,25 @@ const connection = mysql.createConnection({
   password: 'ReallyCoolSecurePassword!123',
   database: 'ecommerce_db'
 });
-
+//show that our connection succeeded
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
 
+//execute some queries
+connection.query('SELECT * FROM user', (err,rows) => {
+    if(err) throw err;
+  
+    console.log('Data received from Db:');
+    console.log(rows);
+  });
+
+
+
+
+//closing connection
+connection.end((err) => {
+    //closes the connection gracefully
+});
 
